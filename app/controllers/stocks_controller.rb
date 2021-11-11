7ddc42144c028b6a52ecf3ff29b6c7993c2ee25b
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-11-10 23:56:00 -0500

    add ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,13 +3,15 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
+        flash[:alert] = 'Please enter a valid symbol to search'
         redirect_to my_portfolio_path
       end
     else
-      flash[:alert] = "Please enter a symbol for a search"
+      flash[:alert] = 'Please enter a symbol for a search'
       redirect_to my_portfolio_path
     end
   end
